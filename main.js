24e586f1888d627834fb23f0380a00b85f02b880
Mohamed Roshdy <62522577+PASHAMESER@users.noreply.github.com>
2022-11-07 16:27:42 +0200

    up

---
+++ main.js
@@ -0,0 +1,22 @@
+let btnSearch = document.getElementById("btnSearch")
+let iconSearch = document.getElementById("iconSearch")
+let button = document.getElementById("button")
+
+window.onload = function () {
+
+  setTimeout(()=>{
+    button.click()
+  },4000)
+
+
+  // if (btnSearch.focus()) {
+  //   // iconSearch.classList.add("displayToggle")
+  //   iconSearch.classList.add("displayToggle")
+  //   console.log("add")
+  // }else {
+  //   // iconSearch.classList.remove("displayToggle")
+  //   // iconSearch.classList.add("displayToggle")
+  //   console.log("re")
+  // }
+  
+}
