4228ed8732d7bb97da5415d6b8d1d0471d536a15
Mohamed Roshdy <62522577+PASHAMESER@users.noreply.github.com>
2022-11-24 13:08:06 +0200

    up

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let btnSearch = document.getElementById("btnSearch")
 let iconSearch = document.getElementById("iconSearch")
 let button = document.getElementById("button")
-
+let imgUser = document.querySelectorAll(".boxImg img")[0]
 window.onload = function () {
 
   //  setTimeout(()=>{
@@ -27,6 +27,7 @@ let divcontent = document.querySelector(".divcontent")
 let boxImg = document.querySelector(".boxImg")
 
 bigBoxComment.addEventListener('mouseover' , function () {
+imgUser.style.cssText = `border: none;`
   boxImg.classList.add("classBorderSolid")
   divcontent.classList.add("classBorderSolid")
   boxImg.classList.add("classImgSize")
